992b8728df7f57327563a05a54b304d9604afc39
ayon121 <borshonsaha1234@gmail.com>
2025-05-08 19:58:05 +0600

    problem three solved

--- problem.ts
+++ problem.ts
@@ -2,7 +2,7 @@
 
 
 
-// Problem 1:
+// -----------------Problem 1
 // Description: Create a function that takes a string and an optional boolean.
 // If the boolean is true or not provided, return the string in uppercase.
 // If the boolean is false, return the string in lowercase.
@@ -21,7 +21,7 @@ console.log(formatString("Hello", false))    // Output: "hello"
 
 
 
-// Problem 2:
+//-------------------Problem 2
 // Description: Create a function that filters an array of objects by the rating property,
 // returning only items with a rating of 4 or more.
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
@@ -46,8 +46,28 @@ console.log(filterByRating(books)) // Output: [ { title: "Book A", rating: 4.5 }
 
 
 
+//----------------- Problem 3
+// Description: Create a generic function that concatenates multiple arrays of the same type using rest parameters.
 
-// Problem 4:
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    let result: T[] = [];
+    for (let i = 0; i < arrays.length; i++) {
+        const currentArray = arrays[i];
+
+
+        for (let j = 0; j < currentArray.length; j++) {
+            result.push(currentArray[j]);
+        }
+    }
+    return result;
+}
+
+console.log(concatenateArrays(["a", "b"], ["c"]))     // Output: ["a", "b", "c"]
+console.log(concatenateArrays([1, 2], [3, 4], [5]))    // Output: [1, 2, 3, 4, 5]
+
+
+
+// ----------------Problem 4
 // Description:
 // Create a Vehicle class with private make and year properties and a getInfo() method.
 // Create a Car class extending Vehicle, adding a private model property and a getModel() method.
