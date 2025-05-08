512c053c04c9afdacac9a631d3e07569ebd59f54
ayon121 <borshonsaha1234@gmail.com>
2025-05-08 23:35:11 +0600

    blog2 section completed

--- problem.ts
+++ problem.ts
@@ -1,11 +1,5 @@
-//Assignment One
 
 
-
-// -----------------Problem 1
-// Description: Create a function that takes a string and an optional boolean.
-// If the boolean is true or not provided, return the string in uppercase.
-// If the boolean is false, return the string in lowercase.
 
 function formatString(text: string, toUpperCase: boolean = true): string {
     return toUpperCase ? text.toUpperCase() : text.toLowerCase();
@@ -21,9 +15,7 @@ console.log(formatString("Hello", false))    // Output: "hello"
 
 
 
-//-------------------Problem 2
-// Description: Create a function that filters an array of objects by the rating property,
-// returning only items with a rating of 4 or more.
+
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     const filteredItems: { title: string; rating: number }[] = [];
     for (let i = 0; i < items.length; i++) {
@@ -46,8 +38,7 @@ console.log(filterByRating(books)) // Output: [ { title: "Book A", rating: 4.5 }
 
 
 
-//----------------- Problem 3
-// Description: Create a generic function that concatenates multiple arrays of the same type using rest parameters.
+
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
     let result: T[] = [];
@@ -71,10 +62,8 @@ console.log(concatenateArrays([1, 2], [3, 4], [5]))    // Output: [1, 2, 3, 4, 5
 
 
 
-// ----------------Problem 4
-// Description:
-// Create a Vehicle class with private make and year properties and a getInfo() method.
-// Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+
+
 class Vehicle {
     constructor(
         private make: string,
@@ -112,11 +101,10 @@ console.log(myCar.getModel());  // Output: "Model: Corolla"
 
 
 
-// ----------------Problem 5:
-// Description: 
-// Write a function that takes a string | number and returns:
-// The length if it's a string
-// The number multiplied by 2 if it's a number
+
+
+
+
 function processValue(value: string | number): number {
     if (typeof value === "string") {
         return value.length;
@@ -135,10 +123,10 @@ console.log(processValue(10));      // Output: 20
 
 
 
-//--------------- Problem 6
-// Description:
-// Define an interface Product and create a function to 
-// find the product with the highest price in an array. If the array is empty, return null.
+
+
+
+
 interface Product {
     name: string;
     price: number;
@@ -169,10 +157,9 @@ console.log(getMostExpensiveProduct(products))    // Output: { name: "Bag", pric
 
 
 
-//----------- Problem 7
-// Description:
-// Define an enum Day for the days of the week.
-// Create a function that returns "Weekday" or "Weekend" based on the input day.
+
+
+
 enum Day {
     Monday,
     Tuesday,
@@ -198,10 +185,10 @@ console.log(getDayType(Day.Sunday)) // Output: "Weekend"
 
 
 
-//----------- Problem 8
-// Description: Create an async function that:
-// Returns the square of a number after 1 second
-// Rejects if the number is negative
+
+
+
+
 async function squareAsync(n: number): Promise<number> {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
