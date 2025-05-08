646add5758d0472473786da5aebb2db84874dfd9
ayon121 <borshonsaha1234@gmail.com>
2025-05-08 20:35:13 +0600

    problem eight  solved

--- problem.ts
+++ problem.ts
@@ -169,7 +169,7 @@ console.log(getMostExpensiveProduct(products))    // Output: { name: "Bag", pric
 
 
 
-// Problem 7:
+//----------- Problem 7
 // Description:
 // Define an enum Day for the days of the week.
 // Create a function that returns "Weekday" or "Weekend" based on the input day.
@@ -192,3 +192,27 @@ function getDayType(day: Day): string {
 }
 console.log(getDayType(Day.Monday))  // Output: "Weekday"
 console.log(getDayType(Day.Sunday)) // Output: "Weekend"
+
+
+
+
+
+
+//----------- Problem 8
+// Description: Create an async function that:
+// Returns the square of a number after 1 second
+// Rejects if the number is negative
+async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (n < 0) {
+                reject("Error: Negative number not allowed")
+            } else {
+                resolve(n * n);
+            }
+        }, 1000)
+    });
+}
+
+squareAsync(4).then(console.log);        // Output after 1s: 16
+squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
